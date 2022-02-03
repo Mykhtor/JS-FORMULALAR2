05e3339608d0b2663859e405f5a83eab22cfd5fb
Mykhtor <muxtordev@gmail.com>
2022-02-03 09:09:03 +0500

    JS-FORMULALAR2

---
+++ app.js
@@ -0,0 +1,116 @@
+// let a = eval('45+15-87/3')
+// alert( a )
+
+// FORMULALAR
+
+// 1
+
+// a)
+// let x = 2
+// let y = 7*x+3*x+6
+// alert(y)
+
+// b)
+
+// let a = 2
+// let x = 12*a+7*a+12
+// alert(x)
+
+
+
+// 2
+
+// let a = prompt('Kvadrat tomonini kiriting:')
+// let p = a*4
+// alert( p )
+
+// 3
+
+// let r = prompt('Aylana radiusini kiriting:')
+// // let pi = 3.14
+// // let c = 2*pi*r*
+// let d = 2*r
+// alert(d)
+
+
+// // 4
+// let r = prompt('Aylana radiusini kiriting:')
+// // c-? s=?
+// // c=2*pi*r
+// // s=pi*r kv
+// let pi = 3.14
+// let c = 2*pi*r
+// let s = pi*r**2
+// alert('aylana uzunligi ' + c + '\n' + 'aylana yuzi ' + s)
+
+// 5
+// a)
+// let a =Number(prompt('a sonini kiriting:'))
+// let b =Number(prompt('b sonini kiriting:'))
+// let ortaArifmetik = (a + b)/2
+// alert( ortaArifmetik )
+
+// b)
+// let a = prompt('Son kiriting:')
+// let b = prompt('Son kiriting:')
+// let c = prompt('Son kiriting:')
+// let ortaGeometrik = ((a*b*c)**1/3)
+// alert( ortaGeometrik )
+
+// 6
+// let a = prompt('Yuzini topish uchun a tomonini kiriting:')
+// let b = prompt('Yuzini topish uchun  b tomonini kiriting:')
+// let S = a*b
+
+// let x = Number(prompt('Perimetrini topish uchun x tomonini kiriting:'))
+// let y = Number(prompt('Perimetrini topish uchun y tomonini kiriting:'))
+// let P = 2*(x*y)
+// alert( "To'g'ri to'rtburchakning yuzi - " + S + ' \n' + "To'g'ri to'rtburchakning perimetri - " + P )
+
+// 7
+// let dEni = prompt('Devor enini kiriting:')
+// let dBoyi = prompt("Devor bo'yini kirting:")
+// let dYuzi = dEni * dBoyi
+// alert( 'Devor yuzi ' + dYuzi + ' kv metrga teng')
+
+// let gEni = prompt("G'isht enini kiriting: ")
+// let gBoyi = prompt("G'isht bo'yini kiriting: ")
+// let gYuzi = gEni * gBoyi
+// alert("G'ishtning yuzi " + gYuzi + ' kv smga teng ')
+
+// let gSoni = dYuzi / gYuzi
+// alert(dYuzi + "kv metr devor ko'tarish uchun " + gSoni + " ta g'isht kerak ")
+
+
+// 8
+// let A = Number(prompt('A kordinatani kiriting: '))
+// let B = Number(prompt('B kordinatani kiriting: '))
+// // let C = Number(prompt('C kordinatani kiriting: '))
+// // let D = Number(prompt('D kordinatani kiriting: '))
+
+// let S = A*B
+// let P = 2*A+2*B
+// alert("To'rtburchakning perimetri " + P + ' ga ' + '\n' + 'yuzasi esa ' + P + ' ga teng')
+
+// 9
+// let olma = prompt("1 kg olma narxini kiriting ")
+// let xOlma = prompt('Sizga nechi kg olma kerak? ')
+// let jami = olma * xOlma
+// alert(xOlma + " kg olmaning narxi " + jami + " ming so'm bo'ladi")
+
+// 10
+// let olma = prompt('Olma narxini kiriting ')
+// let pechenie = prompt('Pechenie narxini kiriting ')
+// let konfet = prompt('Konfet narxini kiriting ')
+
+// let xOlma = prompt('Nechi kg olma kerak ')
+// let yPechenie = prompt('Nechi kg pechenie kerak ')
+// let zKonfet = prompt('Nechi kg konfet kerak ')
+
+// let olmaJami = olma*xOlma 
+// let pechenieJami = pechenie*yPechenie
+// let konfetJami = konfet*zKonfet
+
+// let jami = olmaJami+pechenieJami+konfetJami
+
+// alert('Jami' + '\n' +  xOlma + ' kg olmaning narxi ' + olmaJami + " so'm " + '\n' + yPechenie + ' kg pecheniening narxi ' + pechenieJami + " so'm " + '\n' + zKonfet + ' kg konfetning narxi ' + konfetJami + " so'm " + '\n' + "Hammasi bo'lib " + jami + " so'm kerak!!!")
